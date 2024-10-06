3a559b38a5c6c0e5d3a347b777baee9aa98d434b
Anakin Childerhose <anakin@childerhose.ca>
2024-10-06 00:03:08 -0400

    bash: added aliases

---
+++ .aliases.sh
@@ -0,0 +1,17 @@
+alias \
+ac='cd ~/.config/awesome' \
+b='brave &> /dev/null &' \
+bt='bluetoothctl connect 84:AD:8D:46:3B:94 &' \
+c='clear' \
+grep='grep --color=auto' \
+gs='git status -s' \
+l='ls -1F' \
+la='ls -laF' \
+ll='ls -l1F' \
+ls='ls --color=auto' \
+pst='pstree -C age' \
+t='tree -CF' \
+ta='tree -aCF' \
+up='cd $(git rev-parse --show-toplevel)' \
+vc='cd ~/.config/nvim/lua/kin/' \
+wire='wireshark &> /dev/null &' \
